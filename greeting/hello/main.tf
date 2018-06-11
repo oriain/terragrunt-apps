73c6d9a1eda5b62a5acbd23ae2cdb9ef7ed54bb8
Ryan Robertson <rjr.devry@gmail.com>
2018-06-11 00:18:31 -0700

    Update greeting app to use v0.1.0 modules.

--- greeting/hello/main.tf
+++ greeting/hello/main.tf
@@ -1,4 +1,4 @@
 module "greeting_hello" {
-  source   = "git@github.com:oriain/terragrunt-modules.git//hello"
+  source   = "git@github.com:oriain/terragrunt-modules.git//hello?ref=v0.1.0"
   greeting = "${var.greeting}"
 }
--- greeting/world/main.tf
+++ greeting/world/main.tf
@@ -1,4 +1,4 @@
 module "greeting_name" {
-  source = "git@github.com:oriain/terragrunt-modules.git//world"
+  source = "git@github.com:oriain/terragrunt-modules.git//world?rev=v0.1.0"
   name   = "${var.name}"
 }
