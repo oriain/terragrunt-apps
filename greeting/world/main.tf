c72c565e05e9348258005685902d0089a9e79856
Ryan Robertson <rjr.devry@gmail.com>
2018-06-11 00:50:15 -0700

    Fix typo.

--- greeting/world/main.tf
+++ greeting/world/main.tf
@@ -1,4 +1,4 @@
-module "greeting_name" {
-  source = "git@github.com:oriain/terragrunt-modules.git//world?rev=v0.1.0"
+module "greeting_world" {
+  source = "git@github.com:oriain/terragrunt-modules.git//world?ref=v0.1.0"
   name   = "${var.name}"
 }
